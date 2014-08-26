f9f34730a48e845447557fd69889cdd08ef9424f
Bruce Durling <bruce@mastodonc.com>
2014-08-26 14:51:44 +0100

    Decomplected name from id.
    This fixes a bug where a space was introduced into the id.

--- site/js/tabs.js
+++ site/js/tabs.js
@@ -1,11 +1,11 @@
 
 var render_tabs = function render_tabs() {
 
-    var tabs= [ {"name": "Diabetes", "pathname": "/diabetes.html"},
-                {"name": "Healthy Places", "pathname": "/toothpick.html"} ];
+    var tabs= [ {"id": "Diabetes", "name": "Diabetes", "pathname": "/diabetes.html"},
+                {"id": "HealthyPlaces", "name": "Healthy Places", "pathname": "/toothpick.html"} ];
 
-    var add_click = function add_click(name, pathname) {
-        $('#' + name).click(function (e) {
+    var add_click = function add_click(id, pathname) {
+        $('#' + id).click(function (e) {
             window.location.pathname = pathname;
         });
     };
@@ -13,17 +13,18 @@ var render_tabs = function render_tabs() {
     var container = $('#project-tabs');
 
     for (i = 0; i < tabs.length; i++) {
+        var id = tabs[i]['id'];
         var name = tabs[i]['name'];
         var pathname = tabs[i]['pathname'];
 
-        var clazz= (window.location.pathname == pathname) ? "active" : "inactive";
+        var clazz = (window.location.pathname == pathname) ? "active" : "inactive";
 
         container.append('<li class="' + clazz + '">' +
-                         '<a href="#" role="tab" data-toggle="tab" id="' + name + '">'
+                         '<a href="#" role="tab" data-toggle="tab" id="' + id + '">'
                          + name +
                          '</a></li>');
 
-        add_click(name, pathname);
+        add_click(id, pathname);
 
     };
 
